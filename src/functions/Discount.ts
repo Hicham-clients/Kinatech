d9c9c3fb78d59135df50fd1d20a4823aa3ab93eb
HICHAM <elloutfihicham@gmail.com>
2025-12-18 16:53:51 +0100

    sd

--- src/functions/Discount.ts
+++ src/functions/Discount.ts
@@ -1,10 +1,12 @@
 export function calculNewPrice(discount:(number),oldPrice:number){
-return Number((oldPrice-(oldPrice*(discount/100))))
+// return Number((oldPrice-(oldPrice*(discount/100)))) 
+return discount+oldPrice
 
 }
 export function PriceFormat(price: number|string) {
-  return price.toLocaleString('fr-FR', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  })
+  // return price.toLocaleString('fr-FR', {
+  //   minimumFractionDigits: 2,
+  //   maximumFractionDigits: 2,
+  // })
+  return price
 }
