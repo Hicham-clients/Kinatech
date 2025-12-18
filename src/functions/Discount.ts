8890f7dba76d05f08037d19deac9976849ed107f
HICHAM <elloutfihicham@gmail.com>
2025-12-18 12:50:36 +0100

    edit price format for promos

--- src/functions/Discount.ts
+++ src/functions/Discount.ts
@@ -1,11 +1,14 @@
 export function calculNewPrice(discount:(number),oldPrice:number){
-return Number((oldPrice-(oldPrice*(discount/100)))).toFixed(2).replace('.',',')
-
+return Number((oldPrice-(oldPrice*(discount/100)))).toLocaleString('fr-FR',{
+    minimumFractionDigits:2,
+    maximumFractionDigits:2
+})
+// .toFixed(2).replace('.',',')
 
 }
 export function PriceFormat(price: number) {
-  return new Intl.NumberFormat('fr-FR', {
+  return price.toLocaleString('fr-FR', {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
-  }).format(price)
+  })
 }
