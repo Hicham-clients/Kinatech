22821c4f7e31c0c210398e5cf3aac6266ac58c8f
HICHAM <elloutfihicham@gmail.com>
2025-12-20 12:20:37 +0100

    sds

--- src/app/products/[slug]/_components/PageDetail.tsx
+++ src/app/products/[slug]/_components/PageDetail.tsx
@@ -1,26 +1,21 @@
 "use client";
-import { Product, useDetails } from "@/hooks/useDetail";
+import { useDetails } from "@/hooks/useDetail";
 import DetailComponent from "./DetailComponent";
-import { notFound, useParams } from "next/navigation";
+import { useParams } from "next/navigation";
 import DetailSkeleton from "@/skeletons/DetailLoading";
-// import Refetch from "@/components/Refetch";
-type props={
-  data:Product|null
-}
-const PageDetail = ({data}:props) => {
-  // const { slug }: { slug: string } = useParams(); 
-  const isLoading=false
-  const error=false
-  // const { data, isLoading, error, refetch } = useDetails(slug);
+import Refetch from "@/components/Refetch";
+
+const PageDetail = () => {
+  const { slug }: { slug: string } = useParams();
+  const { data, isLoading, error, refetch } = useDetails(slug);
   if (isLoading) {
     return <DetailSkeleton />;
   }
   if (error) {
     return (
-      // <div className="flex h-[50vh] flexCenter">
-      //   <Refetch onclick={refetch} />
-      // </div> 
-      notFound()
+      <div className="flex h-[50vh] flexCenter">
+        <Refetch onclick={refetch} />
+      </div>
     );
   }
 
--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,40 +1,13 @@
 
-import { Product } from "@/hooks/useDetail";
 import PageDetail from "./_components/PageDetail"
-import { notFound } from "next/navigation";
 // export const generateMetadata=async({params}:{params:{slug:string}})=>{
   
 // return {
 //   title:params.slug.split('-').join(' ')
 // }
-// } 
-export async function getProduct(slug: string): Promise<Product | null> {
-  try {
-    const res = await fetch(
-      `https://kinatech.ma/admin/public/api/products/${slug}`,
-      {
-        next: { revalidate: 60 }, // ISR
-      }
-    );
-
-    if (!res.ok) {
-      console.warn('Product not found:', slug);
-      return null; // ما تطيّحش build
-    }
-
-    return await res.json();
-  } catch (error) {
-    console.error('getProduct error:', error);
-    return null; // fallback
-  }
-}
-
-type params={
-  slug:string
-}
-const Detail =async({slug}:params) => { 
-  const data=await getProduct(slug)
+// }
+const Detail = () => {
   return (
-<PageDetail data={data}/>  )
+<PageDetail/>  )
 }
 export default Detail
