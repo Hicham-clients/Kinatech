8a23801a548269321fdfdf1f3b4e6541c9346271
HICHAM <elloutfihicham@gmail.com>
2025-12-20 18:40:40 +0100

    ss

--- src/app/products_categories/_components/FilterServer.tsx
+++ src/app/products_categories/_components/FilterServer.tsx
@@ -1,42 +1,44 @@
 import Filter from "./Filter";
 
-async function getCategories() { 
-    const res = await fetch(
-    `https://kinatech.ma/admin/public/api/categories`,
-    {
-      next: { revalidate: 60 }, 
-    }
-  );
+// async function getCategories() { 
+//     const res = await fetch(
+//     `https://kinatech.ma/admin/public/api/categories`,
+//     {
+//       next: { revalidate: 60 }, 
+//     }
+//   );
 
-  if (!res.ok) {
-  console.log('error');
+//   if (!res.ok) {
+//   console.log('error');
   
-  }
+//   }
 
-  return res.json();
+//   return res.json();
   
-} 
-async function getBrands() { 
-    const res = await fetch(
-    `https://kinatech.ma/admin/public/api/brands`,
-    {
-      next: { revalidate: 60 }, 
-    }
-  );
+// } 
+// async function getBrands() { 
+//     const res = await fetch(
+//     `https://kinatech.ma/admin/public/api/brands`,
+//     {
+//       next: { revalidate: 60 }, 
+//     }
+//   );
 
-  if (!res.ok) {
-  console.log('error');
+//   if (!res.ok) {
+//   console.log('error');
   
-  }
+//   }
 
-  return res.json();
+//   return res.json();
   
-} 
+// } 
 const FilterServer = async() => { 
-    const categories=await getCategories() 
-    const brands=await getBrands()
+    // const categories=await getCategories() 
+    // const brands=await getBrands()
   return (
-<Filter data={categories} brands={brands}/>
+<Filter/>
+//  data={categories} brands={brands}
+ 
 )
 }
 export default FilterServer
