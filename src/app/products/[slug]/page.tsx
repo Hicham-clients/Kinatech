968ea5671774133d6cd73854575dbf17a4d918a3
HICHAM <elloutfihicham@gmail.com>
2025-12-20 17:29:46 +0100

    best practice

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,11 +1,40 @@
 
 import { notFound } from "next/navigation"
 import PageDetail from "./_components/PageDetail"
+import { Product } from "@/hooks/useCategories"
 type Props={
   params:{
     slug:string
   }
 } 
+export async function generateStaticParams() {
+  const res = await fetch("https://kinatech.ma/admin/public/api/products")
+  const products: Product[] = await res.json()
+
+  return products.map((p) => ({
+    slug: p.slug
+  }))
+}
+//SEO 
+export async function generateMetadata({ params }: Props) {
+  const product = await getProduct(params.slug)
+
+  return {
+    title: product?.Title || "Produit Kinatech",
+    description: product?.description || "Découvrez notre produit sur Kinatech",
+    openGraph: {
+      title: product?.Title || "Produit Kinatech",
+      description: product?.description || "Découvrez notre produit sur Kinatech",
+      images: [
+        {
+          url: product?.photo || "/images/kinatech/logo.png",
+          width: 800,
+          height: 600,
+        },
+      ],
+    },
+  }
+}
 
 const getProduct=async(slug:string)=>{
   const response=await fetch(`https://kinatech.ma/admin/public/api/products/${slug}`,{
