a8c47deeda392713c847267cbc1a64164a897f0a
Hycham17 <elloutfihicham@gmail.com>
2026-02-12 01:10:28 +0100

    dd

--- src/components/partials/Menu/Menu.tsx
+++ src/components/partials/Menu/Menu.tsx
@@ -366,7 +366,7 @@ const Menu = ({ data }: Props) => {
                       {child.products?.map((pro: Product) => (
                         <Link
                           key={pro.id}
-                          className="text-main-hover block truncate "
+                          className="text-main-hover block text-sm "
                           href={`/products/${pro.url}`}
                         >
                           {pro.slug}
