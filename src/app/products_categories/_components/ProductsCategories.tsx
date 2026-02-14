0f45fa493e99ccd079895ac5e75275b571c80ca5
Hycham17 <elloutfihicham@gmail.com>
2026-02-14 13:05:17 +0100

    improve

--- src/app/products_categories/_components/ProductsCategories.tsx
+++ src/app/products_categories/_components/ProductsCategories.tsx
@@ -1,7 +1,11 @@
 "use client";
 import PageTitle from "@/components/PageTitle";
 import ProductCard from "./ProductCard";
-import { PaginatedResponse, Product, useCategories } from "@/hooks/useCategories";
+import {
+  PaginatedResponse,
+  Product,
+  useCategories,
+} from "@/hooks/useCategories";
 import { useRouter, useSearchParams } from "next/navigation";
 import Paginate from "./Paginate";
 import { ReactNode, useState } from "react";
@@ -10,22 +14,22 @@ import clsx from "clsx";
 // import Refetch from "@/components/Refetch";
 type Props = {
   children?: ReactNode;
-  data:PaginatedResponse
+  data: PaginatedResponse;
 };
-const ProductsCategories = ({children,data}:Props) => {
+const ProductsCategories = ({ children, data }: Props) => {
   const searchParams = useSearchParams();
-  const [currentPage, setCurrentPage] = useState(Number(searchParams.get('page'))||1);
+  const [currentPage, setCurrentPage] = useState(
+    Number(searchParams.get("page")) || 1,
+  );
   // const { data, isLoading, error, refetch } = useCategories(
   //   `${searchParams.toString()}&page=${currentPage}`
   // );
-  const router=useRouter()
+  const router = useRouter();
   const handlePaginateClick = (event: { selected: number }) => {
     setCurrentPage(event.selected + 1);
-       const params = new URLSearchParams(searchParams);
-         params.set("page",(event.selected + 1).toString());
-       router.push(`/products_categories?${params.toString()}`
-      
-      )
+    const params = new URLSearchParams(searchParams);
+    params.set("page", (event.selected + 1).toString());
+    router.push(`/products_categories?${params.toString()}`);
     window.scrollTo({ left: 0, top: 0 });
   };
   return (
@@ -33,11 +37,12 @@ const ProductsCategories = ({children,data}:Props) => {
       <div className=" flex flex-col gap-y-20 kinatech-container">
         <PageTitle
           title={
-            searchParams.has("brand")
+            (searchParams.has("brand")
               ? searchParams.get("brand")
               : searchParams.has("category")
-              ? searchParams.get("category")
-              : "Nos Produits"
+                ? searchParams.get("category")
+                : "Nos Produits"
+            )?.replaceAll("-", " ") ?? null
           }
           text="Nous serions heureux de vous accompagner dans vos besoins. Découvrez nos produits et profitez d’un service rapide et de qualité."
           // title={searchParams?.toString().length==0?"Toutes les produits":searchParams.has('category')?searchParams.get('category')?.toUpperCase():''}
@@ -56,22 +61,20 @@ const ProductsCategories = ({children,data}:Props) => {
                 ))}{" "}
               </div>
             ) : */}
-            {
-             (data?.data ?? [])?.length > 0 ? (
+            {(data?.data ?? [])?.length > 0 ? (
               <div
                 className={clsx(
-                  "w-full grid sm:grid-cols-2 xl:grid-cols-3 gap-y-14 gap-5"
+                  "w-full grid sm:grid-cols-2 xl:grid-cols-3 gap-y-14 gap-5",
                 )}
               >
-                {data?.data.map((item:Product) => (
+                {data?.data.map((item: Product) => (
                   <ProductCard {...item} key={item.id} />
                 ))}
               </div>
-            )
-            //  : error ? (
-            //   <Refetch onclick={refetch} />
-            // )
-             : (
+            ) : (
+              //  : error ? (
+              //   <Refetch onclick={refetch} />
+              // )
               <div className=" text-center h-full flex justify-center items-center w-full font-A tracking-wider text-xl">
                 Pas des produits
               </div>
