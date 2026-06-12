a247ac580721b27c9b8356a916c23a68a5b9f08e
Hycham17 <fatem.host@gmail.com>
2026-06-13 00:19:45 +0100

    last commit

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
      remotePatterns: [
       {
         protocol: "https",
-        hostname:"kinatech.ma",
+        hostname:"admin.kinatech.ma",
         pathname: "/**",
       },
       
