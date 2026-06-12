e06b48267ce044e8184c7aa597c00917555fc3af
Hycham17 <fatem.host@gmail.com>
2026-06-13 00:25:26 +0100

    sd

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */ 
-  // output:'export'
     images: {
-      
+    
      remotePatterns: [
       {
         protocol: "https",
