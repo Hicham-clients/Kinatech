ed4c6c27538ad3629273c4ff48f73e50b9d426ef
Hycham17 <fatem.host@gmail.com>
2026-07-19 02:06:32 +0100

    ss

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 import Cookies from "js-cookie"; 
 export const urlApi =
-  process.env.NEXT_PUBLIC_API_URL?.replace(/\/+$/, "") || "https://admin.kiantech.ma";
+  process.env.NEXT_PUBLIC_API_URL?.replace(/\/+$/, "") || "https://admin.kinatech.ma";
 export const axiosInstance = axios.create({
   baseURL: urlApi,
   headers: {
